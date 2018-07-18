f7873f11d6abab953927153cc9a0a6501cb04496
Niko Steinhoff <niko@riskident.com>
2018-07-18 20:46:52 +0200

    Refactor scala 'accumulate'
    Swap arguments for consistency between outer and inner function.

--- scala/accumulate/src/main/scala/Accumulate.scala
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -5,12 +5,12 @@ class Accumulate {
   def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
     /** Tail recursive helper function.*/
     @tailrec
-    def recur(acc: List[B])(list: List[A], f: A => B): List[B] = list match {
+    def recur(acc: List[B])(f: A => B, list: List[A]): List[B] = list match {
       // `::` is a constant time operation on lists, but it reverses the input.
-      case x :: xs => recur(f(x) :: acc)(xs, f)
+      case x :: xs => recur(f(x) :: acc)(f, xs)
       case Nil => acc.reverse
     }
 
-    recur(List())(list, f)
+    recur(List())(f, list)
   }
 }
