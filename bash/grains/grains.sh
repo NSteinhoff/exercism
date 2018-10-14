6723600f46a7025f6a9bc4419f924acb47a92244
Niko Steinhoff <niko@riskident.com>
2018-10-14 18:13:37 +0200

    Add bash 'grains'

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -2,7 +2,14 @@
 
 
 main () {
-    echo "$@"
+    n=$1
+    if [ $n -gt 64  ] || [ $n -lt 1 ]; then
+        echo "Error: invalid input"
+        exit 1
+    else
+        echo $(bc<<<"2 ^ ($n - 1)")
+        exit 0
+    fi
 }
 
 main "$@"
--- bash/grains/grains_test.sh
+++ bash/grains/grains_test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bats
 
 @test "Square 1" {
-  #skip
   run bash grains.sh 1
 
   [ "$status" -eq 0 ]
@@ -9,15 +8,16 @@
 }
 
 @test "Square 2" {
-  #skip
   run bash grains.sh 2
+
+  echo $status
+  echo $output
 
   [ "$status" -eq 0 ]
   [ "$output" = "2" ]
 }
 
 @test "Square 3" {
-  #skip
   run bash grains.sh 3
 
   [ "$status" -eq 0 ]
@@ -25,7 +25,6 @@
 }
 
 @test "Square 4" {
-  #skip
   run bash grains.sh 4
 
   [ "$status" -eq 0 ]
@@ -33,7 +32,6 @@
 }
 
 @test "Square 16" {
-  #skip
   run bash grains.sh 16
 
   [ "$status" -eq 0 ]
@@ -41,7 +39,6 @@
 }
 
 @test "Square 32" {
-  #skip
   run bash grains.sh 32
 
   [ "$status" -eq 0 ]
@@ -49,15 +46,14 @@
 }
 
 @test "Square 64" {
-  #skip
   run bash grains.sh 64
+  echo $output
 
   [ "$status" -eq 0 ]
   [ "$output" = "9223372036854775808" ]
 }
 
 @test "Square 0 causes error" {
-  #skip
   run bash grains.sh 0
 
   [ "$status" -eq 1 ]
@@ -65,7 +61,6 @@
 }
 
 @test "Square -1 causes error" {
-  #skip
   run bash grains.sh -1
 
   [ "$status" -eq 1 ]
@@ -73,7 +68,6 @@
 }
 
 @test "Square 65 causes error" {
-  #skip
   run bash grains.sh 65
 
   [ "$status" -eq 1 ]
